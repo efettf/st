9f2e6d136ed8c81054010ac1513dff6a78fddee3
efettf <efettf@proton.me>
2024-11-10 11:55:54 +0100

    :pushpin: Patch alpha

--- config.def.h
+++ config.def.h
@@ -92,9 +92,9 @@ char *termname = "st-256color";
  *	stty tabs
  */
 unsigned int tabspaces = 8;
-
+ 
 /* bg opacity */
-float alpha = 0.9;
+float alpha = 0.85;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
